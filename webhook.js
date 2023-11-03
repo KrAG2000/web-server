e2cf8ad69b70717bb74d5fac1327230969909146
Kartik Agnihotri <kartik.agnihotri@opensenselabs.com>
2023-11-03 16:12:41 +0530

    Change in webhook.js

--- webhook.js
+++ webhook.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const port = process.env.PORT || 9999;
-const bodyParser = require('body-parser');
 const app = express();
-app.use(bodyParser.json()); 
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname+"/index.html");
   console.log("[INFO] - [200] - Successful!");
 });
 
-
-
-app.post('/webhook', async (req, res) => {
+app.post('/webhook', (req, res) => {
   console.log("----------------START----------------");
 
-  const message = req.body.message;
+  if(message){
+    req.on('data',(data) => {
+      const parsedData = JSON.parse(data)
+      res.send({
+        "text": `You sent the message: >>>${parsedData.message}<<<`
+      });
+    })
 
-  if (message) {
-    res.send({
-      "text": `You sent the message: ${message}`
-    });
-  } else {
+    }
+  else{
     res.send({
       "text": `No message was received!`
     });
