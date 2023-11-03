eb838e0c0e9766ca5d29f589e8297e5b7f6b2472
Kartik Agnihotri <kartik.agnihotri@opensenselabs.com>
2023-11-03 16:22:24 +0530

    Change in webhook.js

--- webhook.js
+++ webhook.js
@@ -4,28 +4,29 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname+"/index.html");
-  console.log("[INFO] - [200] - Successful!");
+  console.log("[INFO] - [200] - GET Successful!");
 });
 
-app.post('/webhook', (req, res) => {
+app.post('/webhook', async (req, res) => {
   console.log("----------------START----------------");
 
-  if(message){
-    req.on('data',(data) => {
-      const parsedData = JSON.parse(data)
-      res.send({
-        "text": `You sent the message: >>>${parsedData.message}<<<`
-      });
-    })
-
-    }
-  else{
-    res.send({
-      "text": `No message was received!`
+  let parsedData;
+  try {
+    const data = await req.buffer();
+    parsedData = JSON.parse(data);
+  } 
+  
+  catch (error) {
+    console.error("[ ERROR ] - [400] - Invalid JSON body!");
+    res.status(400).json({
+      "text": `Invalid JSON body!`
     });
-    console.error("[ ERROR ] - [204] - Message is empty!");
+    return;
   }
 
+  res.status(200).json({
+    "text": `You sent the message: >>>${parsedData.message}<<<`
+  });
   console.log("----------------END----------------");
 });
 
