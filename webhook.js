52bfa2db189f094e757a82e8afde6d61bf4012c4
Kartik Agnihotri <kartik.agnihotri@opensenselabs.com>
2023-11-06 13:43:16 +0530

    Major Change V2.0.0

--- webhook.js
+++ webhook.js
@@ -1,52 +1,21 @@
-// const express = require('express');
-// const port = process.env.PORT || 9999;
-// const app = express();
-
-
-// app.post('/webhook', (req, res) => {
-//   console.log("----------------START----------------");
-
-//   req.on('data',(data) => {
-//     if(data || data != "") {
-//       const parsedData = JSON.parse(data);
-//       console.log(parsedData);
-
-//       let response = '';
-//       for (const [key, value] of Object.entries(parsedData)) {
-//         response += `${key}: ${value}, `;
-//       }
-
-//       res.send({
-//         "message": response
-//       });
-//       for (const key in parsedData) {
-//         console.log(key, " ---------- ", parsedData[key]);
-//       }
-//       console.log(parsedData);
-//     }
-//     else{
-//       res.send({
-//         "message": 'No message has been received!'
-//       });
-//       console.error("[ ERROR ] - [204] - Message is empty!");
-//     }
-//   })
-
-//   console.error("[ ERROR ] - [204] - Message is empty!");
-
-//   console.log("----------------END----------------");
-// });
-
-// app.listen(port, () => {
-//   console.log(`[ INFO ] - [200] - Listening to ${port} server successful!`);
-// });
-
-
 const axios = require('axios');
 const unomiUrl = 'http://localhost:8181/cxs';
 const express = require('express');
+
 const port = process.env.PORT || 9999;
+
 const app = express();
+
+function generateUUID() {
+  let id = '';
+  for (let i = 0; i < 24; i++) {
+    if (i === 4 || i === 8 || i === 12 || i === 16 || i === 20) {
+      id += '-';
+    }
+    id += Math.floor(Math.random() * 24).toString(24);
+  }
+  return id;
+}
 
 app.post('/webhook', async (req, res) => {
   console.log("----------------START----------------");
@@ -54,48 +23,70 @@ app.post('/webhook', async (req, res) => {
   req.on('data', async (data) => {
     if (data && data !== '') {
       const parsedData = JSON.parse(data);
-      console.log(parsedData);
+      // console.log(parsedData);
 
-      const profileData = { "main": parsedData, "additionals": ["UUID", "SID", "PID", "TS"] };
+      const profileData = { "intent": ["positive", "negative", "neutral", "unknown"], "main": parsedData};
       try {
         const httpClient = axios.create({
           baseURL: unomiUrl,
           headers: {
+            // BELOW LINE IS VERY IMPORTANT AS WITHOUT AUTHENTICATION, UNOMI WILL NOT ALLOW YOU TO USE REST API TO POST ANYTHING! 
             'Authorization': 'Basic a2FyYWY6a2FyYWY=',
             'Content-Type': 'application/json',
           },
 
         });
 
-        const profileRequest = {
-          itemId: new Date(),
-          itemType: 'profile',
-          properties: profileData,
-        };
+        const profileSent = {
+          "itemId": generateUUID(),
+          "itemType": "profile",
+          "version": 1,
+          "properties": {
+            "nbOfVisits": 1,
+            "lastVisit": new Date(),
+            "firstVisit": new Date(),
+            "body": profileData
+          },
+          "systemProperties": {
+            "lastUpdated": new Date()
+          },
+          "segments": ["Hello", "World"],
+          "scores": {},
+          "consents": {
+            "mark1_test": {
+              "scope": "string",
+              "typeIdentifier": "string",
+              "status": "GRANTED",
+              "statusDate": new Date(),
+              "revokeDate": new Date(),
+              "consentGrantedNow": true
+            }
+          }
+        }
 
-        const response = await httpClient.post('/profiles', profileRequest);
+        const response = await httpClient.post('/profiles', profileSent);
 
         if (response.status === 201 || response.status === 200) {
-          console.log(profileRequest);
-          console.log(`Profile ID: ${profileRequest.itemId} created successfully`);
-          res.send(`[SUCCESS] - PID: ${profileRequest.itemId}`);
-        } else {
-          console.error('Profile creation failed:', response.statusText);
-          res.send("Profile creation failed!");
+          res.send(`[SUCCESS] - PID: ${profileSent.itemId}`);
+          console.log(`[SUCCESS] - PID: ${profileSent.itemId}`);
         }
-      } catch (error) {
-        console.error('Error creating profile:', error.message);
-        res.send("Error creating profile!");
+        else {
+          res.send(`[ERROR] - [48] - Profile creation failed: ${response.statusText}`);
+          console.error(`[ERROR] - [48] - Profile creation failed: ${response.statusText}`);
+        }
+      }
+      catch (error) {
+        res.send(`[ERROR] - [53] - Error creating profile: ${error.message}`);
+        console.error(`[ERROR] - [53] - Error creating profile: ${error.message}`);
       }
     }
     else {
       res.send({
-        message: 'No message has been received!',
+        message: "No message found in the request!",
       });
-      console.error('[ ERROR ] - [204] - Message is empty!');
+      console.error("No message found in the request!");
     }
   });
-  console.log("----------------END----------------");
 });
 
 app.listen(port, () => {
