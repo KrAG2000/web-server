6226e9176303323e0cc8d6adcfa39e4760cd3d88
Kartik Agnihotri <agni5kartik@gmail.com>
2024-01-08 13:52:00 +0530

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -17,10 +17,10 @@ app.get("/", (req, res) => {
 
 app.post('/webhook', async (req, res) => {
   console.log("----------------START----------------");
-  console.log(req);
+  console.log(req.body["messenger user id"]);
   // console.log(req.body["first name"]);
   console.log("----------------END----------------");
-);
+});
 
 app.listen(port, () => {
   console.log(`[ INFO ] - [200] - Listening to ${port} server successful!`);
