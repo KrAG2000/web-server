59b835d6050842a61fde49115eb779177037b694
Kartik Agnihotri <agni5kartik@gmail.com>
2024-01-08 17:06:49 +0530

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -19,7 +19,7 @@ app.post('/webhook', async (req, res) => {
   console.log("----------------START----------------");
   console.log({req.headers.email : req.body});
   console.log("----------------END----------------");
-  res.send("SUCCESS!");
+  res.send(req.body);
 });
 
 app.listen(port, () => {
