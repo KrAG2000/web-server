022a9c259b5a31b0dde84641ec64208418cb3c29
Kartik Agnihotri <agni5kartik@gmail.com>
2024-01-09 22:39:04 +0530

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,14 +1,12 @@
-const axios = require('axios');
 const path = require('path');
 const express = require('express');
-// const unomiUrl = 'http://localhost:9200/cxs';
-const unomiUrl = 'https://dxp-core.oslabs.app/cxs';
-// const essUrl = 'https://dxp-middleware.oslabs.app/ess';
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 9999;
 
 const app = express();
 app.use(express.static(path.join(__dirname, '/')));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -16,11 +14,11 @@ app.get("/", (req, res) => {
 });
 
 app.post('/webhook', (req, res) => {
-  console.log("----------------START----------------");
-  console.log(req.body);
-  console.log("----------------END----------------");
-  res.send({"statusCode": "It's 204, No data!"});
-});
+    console.log("----------------START----------------");
+    console.log(req.body);
+    console.log("----------------END----------------");
+    res.send({"statusCode": "It's 204, No data!"});
+  });
 
 app.listen(port, () => {
   console.log(`[ INFO ] - [200] - Listening to ${port} server successful!`);
